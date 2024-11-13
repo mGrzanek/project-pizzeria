33635ad490c34596bf71d9c929f09105bcd4d69e
Monika Grzanek <grzanekmonika@o2.pl>
2024-11-13 13:13:57 +0100

    Add class Product with constructor, add initData and initMenu methods to app

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,29 @@ const select = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      console.log('new Product: ', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu:  function() {
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    }, 
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +82,9 @@ const select = {
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
