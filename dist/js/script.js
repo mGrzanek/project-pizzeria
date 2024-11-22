762f2e712dce442abef57af008fb53071fb8c8d3
Monika Grzanek <grzanekmonika@o2.pl>
2024-11-22 11:01:50 +0100

    Add remove method to CartProduct class

--- dist/js/script.js
+++ dist/js/script.js
@@ -431,6 +431,18 @@ const select = {
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
     }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event =  new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
   }
 
   const app = {
